dfceef0c24603b53d5c23797f364203a19522378
Alex <ax-holzmann@gmx.de>
2017-11-06 16:24:50 +0100

    Renamed all variables. Variable types deleted in variable names

--- ClassesAndObj.cpp
+++ ClassesAndObj.cpp
@@ -13,30 +13,35 @@
 
 using namespace std;
 
-vector<Person> vecPersDir;
-vector<address> vecAddress;
+vector<Person> person_directory;
+vector<address> address_directory;
 
 bool bSort(Person a, Person b)
 {
-   bool bReturn;
-   char chA, chB = ' ';
-   chA = a.get_Name().front();
-   chB = b.get_Name().front();
+   bool sorted_return;
+   char firstletter_a = ' '; 
+   char firstletter_b = ' ';
+   firstletter_a = a.get_Name().front();
+   firstletter_b = b.get_Name().front();
 
    for (unsigned int i=0; i<a.get_Name().length(); i++)
    {
-      if (chA <= chB) bReturn=true;
-      else bReturn=false;
+      if (firstletter_a <= firstletter_b) sorted_return=true;
+      else sorted_return=false;
    }
 
-   return bReturn; 
+   return sorted_return; 
 }
 
 int main()
 {  
-   string sInpName, sInpPhone, sInpStreet, sInpPostCode, sInpCountry = "";
-   char chInput = '0';
-   bool bEingabe = true;
+   string inp_name = ""; 
+   string inp_phone_mobile = "";
+   string inp_street = "";
+   string inp_postcode = "";
+   string inp_country = "";
+   char selection = '0';
+   bool entry = true;
    ofstream outputFile;
 
    do
@@ -49,78 +54,78 @@ int main()
       cout<<"5 = Export to txt"<<endl;
       cout<<"6 = Import from txt"<<endl;
       cout<<"7 = Ende"<<endl;
-      cin>>chInput;
-      if(chInput == '1')   //Input data
+      cin>>selection;
+      if(selection == '1')   //Input data
       {
-         bool bTemp = false;
-         char chTemp = ' ';
+         bool temp_state = false;
+         char temp_select = ' ';
          do
          {
             cout<<endl<<"Eingabe Name und Telefon"<<endl<<endl;
             cout<<"Name: ";
             cin.ignore();
-            getline(cin,sInpName);
+            getline(cin,inp_name);
             cin.clear();
             cout<<"Telefon: ";
             cin.ignore(0);
-            getline(cin,sInpPhone);
-            vecPersDir.push_back(Person(sInpName, sInpPhone));
-            std::sort(vecPersDir.begin(), vecPersDir.end(), bSort);
+            getline(cin,inp_phone_mobile);
+            person_directory.push_back(Person(inp_name, inp_phone_mobile));
+            std::sort(person_directory.begin(), person_directory.end(), bSort);
             
             cout<<endl<<"Adresse?  j/n    ";
-            cin>>chTemp;
+            cin>>temp_select;
             cin.clear();
-            if (chTemp != 'j')
+            if (temp_select != 'j')
             {
-               bTemp = true;
+               temp_state = true;
             }
             else
             {
                cout<<endl<<"Strasse: ";
                cin.ignore();
-               getline(cin, sInpStreet);
+               getline(cin, inp_street);
                cin.clear();
                cout<<endl<<"PLZ: ";
                cin.ignore(0);
-               getline(cin, sInpPostCode);
+               getline(cin, inp_postcode);
                cin.clear();
                cout<<endl<<"Stadt: ";
                cin.ignore(0);
-               getline(cin, sInpCountry);
+               getline(cin, inp_country);
 
-               vecAddress.push_back(address(sInpStreet, sInpPostCode, sInpCountry));
+               address_directory.push_back(address(inp_street, inp_postcode, inp_country));
             }
             cin.clear();
             cout<<endl<<"Eingabe weiterer Daten?  j/n"<<endl;
-            cin>>chTemp;
-            if (chTemp != 'j')
+            cin>>temp_select;
+            if (temp_select != 'j')
             {
-               bTemp = true;
+               temp_state = true;
             }
             
-         }while(bTemp == false);
+         }while(temp_state == false);
       }
-      else if(chInput == '2') //Output data
+      else if(selection == '2') //Output data
       {
          cout<<"AUSGABE TELEFONBUCH"<<endl<<endl;
          cout<<"Eintrag:"<<endl<<endl;
-         for (int i=0; i<vecPersDir.size(); i++)
+         for (int i=0; i<person_directory.size(); i++)
          {
-            cout<<vecPersDir.at(i).get_Name()<<endl<<"Tel.: "<<vecPersDir.at(i).get_Phone()<<endl;
+            cout<<person_directory.at(i).get_Name()<<endl<<"Tel.: "<<person_directory.at(i).get_Phone()<<endl;
          }
          cout<<endl;
       }
-      else if(chInput == '3') //Change data
+      else if(selection == '3') //Change data
       {
          cout<<"AUSGABE TELEFONBUCH"<<endl<<endl;
          cout<<"Einträge:"<<endl<<endl;
-         for (int i=0; i<vecPersDir.size(); i++)
+         for (int i=0; i<person_directory.size(); i++)
          {
-            cout<<i<<" : "<<vecPersDir.at(i).get_Name()<< "\t" <<vecPersDir.at(i).get_Phone()<<endl;
+            cout<<i<<" : "<<person_directory.at(i).get_Name()<< "\t" <<person_directory.at(i).get_Phone()<<endl;
          }
          char chName = ' ';
          char chPhone = ' ';
-         char chAddress = ' ';
+         char firstletter_address = ' ';
          int iInput = 0;
          cout<<endl;
          cout<<"Welcher Eintrag soll geändert werden?"<<endl<<endl;
@@ -134,15 +139,15 @@ int main()
          cin>>chPhone;
          cout<<endl<<"Adresse ? j/n => ";
          cin.clear();
-         cin>>chAddress;
+         cin>>firstletter_address;
          if (chName == 'j')
          {
             cout<<endl<<endl<<"Name: ";
             cin.ignore();
-            getline(cin, sInpName);
-            sInpPhone = vecPersDir.at(iInput).get_Phone();
-            vecPersDir.erase(vecPersDir.begin()+iInput);
-            vecPersDir.insert(vecPersDir.begin()+iInput, Person(sInpName, sInpPhone));
+            getline(cin, inp_name);
+            inp_phone_mobile = person_directory.at(iInput).get_Phone();
+            person_directory.erase(person_directory.begin()+iInput);
+            person_directory.insert(person_directory.begin()+iInput, Person(inp_name, inp_phone_mobile));
          }
          if (chPhone == 'j')
          {
@@ -155,12 +160,12 @@ int main()
             { 
                cin.ignore(); 
             }
-            getline(cin, sInpPhone);
-            sInpName = vecPersDir.at(iInput).get_Name();
-            vecPersDir.erase(vecPersDir.begin()+iInput);
-            vecPersDir.insert(vecPersDir.begin()+iInput, Person(sInpName, sInpPhone));
+            getline(cin, inp_phone_mobile);
+            inp_name = person_directory.at(iInput).get_Name();
+            person_directory.erase(person_directory.begin()+iInput);
+            person_directory.insert(person_directory.begin()+iInput, Person(inp_name, inp_phone_mobile));
          }
-         if (chAddress == 'j')
+         if (firstletter_address == 'j')
          {
             cout<<endl<<endl<<"Strasse: ";
             if (chName == 'j' || chPhone == 'j')
@@ -171,24 +176,24 @@ int main()
             {
                cin.ignore();
             }
-            getline(cin,sInpStreet);
+            getline(cin,inp_street);
             cout<<endl<<"PLZ: ";
             cin.ignore(0);
-            getline(cin,sInpPostCode);
+            getline(cin,inp_postcode);
             cout<<endl<<"Stadt: ";
             cin.ignore(0);
-            getline(cin,sInpCountry);
-            vecAddress.erase(vecAddress.begin()+iInput);
-            vecAddress.insert(vecAddress.begin()+iInput, address(sInpStreet, sInpPostCode, sInpCountry));
+            getline(cin,inp_country);
+            address_directory.erase(address_directory.begin()+iInput);
+            address_directory.insert(address_directory.begin()+iInput, address(inp_street, inp_postcode, inp_country));
          }
       }
-      else if(chInput == '4') //Delete data
+      else if(selection == '4') //Delete data
       {
          cout<<"AUSGABE TELEFONBUCH"<<endl<<endl;
          cout<<"Einträge:"<<endl<<endl;
-         for (int i=0; i<vecPersDir.size(); i++)
+         for (int i=0; i<person_directory.size(); i++)
          {
-            cout<<i<<": "<<vecPersDir.at(i).get_Name()<< "\t" <<vecPersDir.at(i).get_Phone()<<endl;
+            cout<<i<<": "<<person_directory.at(i).get_Name()<< "\t" <<person_directory.at(i).get_Phone()<<endl;
          }
          int iInput = 0;
          cout<<endl;
@@ -196,21 +201,21 @@ int main()
          cout<<endl<<"Nummer: ";
          cin.clear();
          cin>>iInput;
-         vecPersDir.erase(vecPersDir.begin()+iInput);
+         person_directory.erase(person_directory.begin()+iInput);
       }
-      else if(chInput == '5') //Export to txt
+      else if(selection == '5') //Export to txt
       {
          outputFile.open ("outputFile.txt");
-         for(int i=0; i<vecPersDir.size(); i++)
+         for(int i=0; i<person_directory.size(); i++)
          {
-            outputFile<<vecPersDir.at(i).get_Name()<< "," <<vecPersDir.at(i).get_Phone();
+            outputFile<<person_directory.at(i).get_Name()<< "," <<person_directory.at(i).get_Phone();
             outputFile<<endl;
          }
          outputFile.close();
       }
-      else if(chInput == '6') //Import from txt
+      else if(selection == '6') //Import from txt
       {
-         string sInpData;
+         string inp_Data;
          int iCntName = 0;
          int iCntPhone = 0;
          int iCntAddress = 0;
@@ -218,44 +223,44 @@ int main()
          ifstream inpFile ("outputFile.txt");
          if (inpFile.is_open())
          {
-            while(getline(inpFile, sInpData))
+            while(getline(inpFile, inp_Data))
             {
-               std::size_t found = sInpData.find(',');
-               std::size_t found2 = sInpData.find('|');
-               std::size_t found3 = sInpData.find(';');
-               std::size_t found4 = sInpData.find('_');
-               sInpName = sInpData.substr(0,found);
-               if(sInpName != "")
+               std::size_t found = inp_Data.find(',');
+               std::size_t found2 = inp_Data.find('|');
+               std::size_t found3 = inp_Data.find(';');
+               std::size_t found4 = inp_Data.find('_');
+               inp_name = inp_Data.substr(0,found);
+               if(inp_name != "")
                {
                   iCntName++;
                }
                iLenght = found2 - found;
-               sInpPhone = sInpData.substr(found + 1, iLenght-1);
-               if(sInpName != "")
+               inp_phone_mobile = inp_Data.substr(found + 1, iLenght-1);
+               if(inp_name != "")
                {
                   iCntPhone++;
                }
                iLenght = found3 - found2;
-               sInpStreet = sInpData.substr(found2 + 1, iLenght-1);
+               inp_street = inp_Data.substr(found2 + 1, iLenght-1);
                iLenght = found4 - found3;
-               sInpPostCode = sInpData.substr(found3 + 1, iLenght-1);
-               sInpCountry = sInpData.substr(found4 + 1);
-               if(sInpCountry != "")
+               inp_postcode = inp_Data.substr(found3 + 1, iLenght-1);
+               inp_country = inp_Data.substr(found4 + 1);
+               if(inp_country != "")
                {
                   iCntAddress++;
                }
-               vecPersDir.push_back(Person(sInpName, sInpPhone));
-               std::sort(vecPersDir.begin(), vecPersDir.end(), bSort);
-               vecAddress.push_back(address(sInpStreet, sInpPostCode, sInpCountry));
+               person_directory.push_back(Person(inp_name, inp_phone_mobile));
+               std::sort(person_directory.begin(), person_directory.end(), bSort);
+               address_directory.push_back(address(inp_street, inp_postcode, inp_country));
             }
             inpFile.close();
             cout<<"Es wurden "<<iCntName<<" Namen, "<<iCntPhone<<" Nummern "<<"und "<<iCntAddress<<" Adressen eingetragen."<<endl;
          }
          else cout<<"Datei kann nicht geöffnet werden.";
       }
-      else if(chInput == '7') //End
+      else if(selection == '7') //End
       {
-         bEingabe = false;
+         entry = false;
       }
       else
       {
@@ -263,5 +268,5 @@ int main()
                    <<"Only 1 - 7!"<<endl<<endl;
       }
 
-   }while(bEingabe == true);
+   }while(entry == true);
 }
